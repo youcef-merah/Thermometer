dabc6d44b9e00b5cd9b4b661f53a26a8d1e2df7a
Youcef Merah <youcef.merah@outlook.com>
2020-01-01 02:22:24 +0100

    First definition of the Thermometer widget
    modified: thermometer.h
              thermometer.cpp
    
    Define the properties, the constructor and paint the widget
    in paintEvent.

--- thermometer.cpp
+++ thermometer.cpp
@@ -1,6 +1,48 @@
 #include "thermometer.h"
+#include <QPainter>
+#include <QDebug>
+#include <QTimer>
 
-Thermometer::Thermometer(QWidget *parent) : QWidget(parent)
+Thermometer::Thermometer(QWidget *parent, double max, double min)
+    :QWidget(parent)
 {
+    setWindowTitle(tr("Temperature"));
+    this->setGeometry(QRect(0, 0, 10, 150));
 
+    currentTemperature = 0.00;
+    maxTemperature = max;
+    minTemperature = min;
+    lowestWarmTemperature  = (2*max + min)/3;
+    highestColdTemperature = (max + 2*min)/3;
+
+    //TODO: level =
+
+    warmColor = QColor(QRgb(0xff4e50));
+    ambiantColor= QColor(QRgb(0x0fd850));
+    coldColor = QColor(QRgb(0x3ab5b0));
+
+    warmColor.setAlpha(warmAlpha = 0xff);
+    ambiantColor.setAlpha(ambiantAlpha = 0xff);
+    coldColor.setAlpha(coldAlpha = 0xff);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout,
+            this, QOverload<>::of(&Thermometer::update));
+    timer->start(1000);
+
+}
+
+void Thermometer::paintEvent(QPaintEvent *)
+{
+    qDebug() << "In paintEvent";
+    QPoint bottom_right(pos().x() + width(), pos().y() + height());
+    QLinearGradient gauge_temp(pos(), bottom_right);
+    gauge_temp.setColorAt(0.0, warmColor);
+    gauge_temp.setColorAt(0.5, ambiantColor);
+    gauge_temp.setColorAt(1, coldColor);
+
+    /* Draw the thermometer */
+    QPainter painter(this);
+    painter.setBrush(QBrush(gauge_temp));
+    painter.drawRoundedRect(QRectF(pos(), bottom_right), 5, 5);
 }
--- thermometer.h
+++ thermometer.h
@@ -7,11 +7,25 @@ class Thermometer : public QWidget
 {
     Q_OBJECT
 public:
-    explicit Thermometer(QWidget *parent = nullptr);
+    explicit Thermometer(QWidget *parent = nullptr,
+                         double maxTemperature = 10,
+                         double minTemperature = -5);
+
+protected:
+    virtual void paintEvent(QPaintEvent *event) override;
 
 signals:
 
 public slots:
+
+private:
+    double currentTemperature;
+    double maxTemperature, minTemperature;
+    double lowestWarmTemperature, highestColdTemperature;
+    QPoint level;
+    QColor warmColor, ambiantColor, coldColor;
+    unsigned int warmAlpha, ambiantAlpha, coldAlpha;
+
 };
 
 #endif // THERMOMETER_H
