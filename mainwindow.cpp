ce37cd2611b7ac5a12b0a4264b48e50bfb5bfe51
Youcef Merah <youcef.merah@outlook.com>
2020-01-28 09:38:50 +0100

    Declare a Thermometer object

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "thermometer.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    ui->setupUi(this);    
+    Thermometer * therm = new Thermometer(this);
+    therm->setCurrentTemperature(7.9);
 }
 
 MainWindow::~MainWindow()
